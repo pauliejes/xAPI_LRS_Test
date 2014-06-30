437a90fc1feadcd404daf0c74680d0ba7d9b438f
Brian J. Miller <brian.miller@tincanapi.com>
2014-06-30 10:06:35 -0500

    Remove prototyping

--- tests/bin/runner.js
+++
@@ -1,6 +0,0 @@
-var spawn = require('child_process').spawn;
-var platform = require('os').platform();
-var cmd = /^win/.test(platform) ? 'bin\\runner.bat' : 'bin/runner.sh';
-spawn(cmd, [], { stdio: 'inherit' }).on('exit', function(code) {
-	process.exit(code);
-});
